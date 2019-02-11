ed99d5477b16c3db5aa7bb6d12bf10189689cf1b
ger204 <gerardmt04@gmail.com>
2019-02-11 20:36:07 +0100

    --help

--- Code/coordenadas.cpp
+++ Code/coordenadas.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
 #include <fstream>
+#include <string.h>
+#include <stdio.h>
 
 using namespace std;
 
 const char DIR_MAPA[] = "E:\\Programing\\C++\\BYME\\Oficial\\COORDENADAS\\Map\\mapa.txt";
+const char DIR_MAPA_1[] = "E:\\Programing\\C++\\BYME\\Oficial\\COORDENADAS\\Map\\mapa1.txt";
 const int VISUAL_CORD = 5;
 
 /*Structs*/
@@ -25,6 +28,7 @@ coordenada pedirCord();
 
 int main(){
   mostrarMapa();
+  enumerarMapa();
   return 0;
 }
 
@@ -74,8 +78,27 @@ void enumerarMapa(){
   ofstream salida;
 
   entrada.open(DIR_MAPA);
-  salida.open("E:\\Programing\\C++\\BYME\\Oficial\\COORDENADAS\\Map\\mapa1.txt");
+  salida.open(DIR_MAPA_1);
+
+  char linea[6];
+
+  if(entrada.good()){
+    std::cout << "ENTRADA EN BUEN ESTADO" << '\n';
+
+      for (int i = 0; i < 6; i++) {
+        entrada.getline(linea,6);
+        std::cout << linea << '\n';
+
+        if (strcmp(linea, "[  ] ") == 0) {
+          std::cout << "HA ENTRADO AL IF" << '\n';
+          salida << "[" << i << " ] ";
+        }
+
+      }
 
 
+     entrada.close();
+     salida.close();
+    }
 
 }
