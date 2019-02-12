f19151cd2b41921a19ceab86c26c72c7fd446b5b
ger204 <gerardmt04@gmail.com>
2019-02-12 18:14:42 +0100

    Añadido mapa de recuperación -mapa2.txt
    Añadida nueva función 
    -addCord() 
    -Falta perfeccionarla 
    -Añade puntos pero borra  
     los ya añadidos

--- Code/coordenadas.cpp
+++ Code/coordenadas.cpp
@@ -10,17 +10,19 @@ const char DIR_MAPA[] = "E:\\Programing\\C++\\BYME\\Oficial\\COORDENADAS\\Map\\m
 const char DIR_MAPA_1[] = "E:\\Programing\\C++\\BYME\\Oficial\\COORDENADAS\\Map\\mapa1.txt";
 const int VISUAL_CORD = 5;
 
+
 /*Structs*/
 
 struct coordenada {
-  int x;
-  int y;
+  char x;
+  char y;
 };
 
 
 /*Funciones*/
 void mostrarMapa();
 void enumerarMapa();
+void addCord(coordenada c);
 void mostrarCord(coordenada c);
 coordenada pedirCord();
 
@@ -28,7 +30,10 @@ coordenada pedirCord();
 /*Main*/
 
 int main(){
+  mostrarMapa();
   enumerarMapa();
+  addCord(pedirCord());
+  mostrarMapa();
   return 0;
 }
 
@@ -86,15 +91,25 @@ void enumerarMapa(){
 
   if(entrada.good()){
 
-                while (entrada.read( (char*) &linea, sizeof(linea)-1 )) {
+                while (entrada.read( (char*) &linea, sizeof(linea)-1 ) ) {
 
-                          if (strcmp(linea,"[  ] " ) == 0) {
+                          if (strcmp(linea,"[  ] " ) == 0 || strcmp(linea,"[  ]\n") == 0) {
                               salida << "[" << i1 << e1 << "] ";
+
+                              if (strcmp(linea,"[  ]\n") == 0) {
+                                salida << endl;
+                              }
+
                             }
-                          else{
-                            salida << "[" << i1 << e1 << "] ";
-                            salida << endl;
+
+                          else if (strcmp(linea,"[++] " ) == 0 || strcmp(linea,"[++]\n") == 0) {
+                            salida << "[++] ";
+                            if (strcmp(linea,"[++]\n") == 0) {
+                              salida << endl;
+                            }
                           }
+
+
 
                             e1++;
                             if (e1==4) {
@@ -107,10 +122,66 @@ void enumerarMapa(){
     }
 
     else {
-      std::cout << "ENTRADA EN MAL ESTADO" << '\n';
+      std::cout << "Error 01: NO se puede leer el ARCHIVO " << DIR_MAPA << '\n';
       entrada.close();
       salida.close();
     }
 
+}
 
+/*Añadir Coordenada*/
+void addCord(coordenada c){
+
+  ifstream entrada;
+  ofstream salida;
+
+  char linea[6];
+  char x = c.x;
+  char y = c.y;
+  char comb1[6] = {'[',x,y,']',' '};
+  char comb2[6] = {'[',x,y,']','\n'};
+  int i1 = 0;
+  int e1 = 0;
+
+  entrada.open(DIR_MAPA_1);
+  salida.open(DIR_MAPA,ios::trunc);
+
+  if (entrada.good()) {
+
+      std::cout << comb1 << '\n';
+
+      while (entrada.read( (char*) &linea, sizeof(linea)-1) ) {
+
+        if (strcmp(linea,comb1) == 0 || strcmp(linea,comb2) == 0) {
+            salida << "[++] ";
+
+            if (strcmp(linea,comb2) == 0) {
+              salida << endl;
+              i1++;
+            }
+
+          }
+
+        else {
+          salida << "[  ] ";
+        }
+
+
+          e1++;
+          if (e1==4) {
+            i1++;
+            e1 = 0;
+            salida << endl;
+          }
+      }
+
+      entrada.close();
+      salida.close();
+  }
+
+  else{
+      std::cout << "Error 02: NO se puede leer el ARCHIVO " << DIR_MAPA << '\n';
+      entrada.close();
+      salida.close();
+  }
 }
