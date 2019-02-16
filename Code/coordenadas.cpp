bcaeeba6d88a8a96d74743c3dfab49e486abd384
ger204 <gerardmt04@gmail.com>
2019-02-16 18:24:48 +0100

    Falta arreglar la función "removeCord()"

--- Code/coordenadas.cpp
+++ Code/coordenadas.cpp
@@ -10,7 +10,7 @@ const char DIR_MAPA[] = "E:\\Programing\\C++\\BYME\\Oficial\\COORDENADAS\\Map\\m
 const char DIR_MAPA_1[] = "E:\\Programing\\C++\\BYME\\Oficial\\COORDENADAS\\Map\\mapa1.txt";
 const char DIR_MAPA_O[] = "E:\\Programing\\C++\\BYME\\Oficial\\COORDENADAS\\Map\\mapa-original.txt";
 const int NUM_MAX = 4;
-const int NUM_MIN = 0;
+const int NUM_MIN = 1;
 
 
 /*Structs*/
@@ -26,6 +26,7 @@ void mostrarMapa();
 void enumerarMapa();
 void formatearMapa();
 void addCord(coordenada c);
+void removeCord(coordenada c);
 void mostrarCord(coordenada c);
 coordenada pedirCord();
 
@@ -35,9 +36,7 @@ coordenada pedirCord();
 int main(){
   formatearMapa();
   mostrarMapa();
-  enumerarMapa();
-  addCord(pedirCord());
-  mostrarMapa();
+
   return 0;
 }
 
@@ -52,7 +51,7 @@ coordenada pedirCord(){
     std::cout << "Punto X: ";
     std::cin >> c.x;
     a = c.x - '0';
-  } while( a > NUM_MAX -1 || a < NUM_MIN);
+  } while( a > NUM_MAX  || a < NUM_MIN);
 
   do {
     std::cout << "Punto Y: ";
@@ -105,13 +104,13 @@ void enumerarMapa(){
   salida.open(DIR_MAPA_1,ios::trunc);
 
   char linea[6];
-  int i1 = 0;
-  int e1 = 0;
+  int i1 = 1;
+  int e1 = 1;
 
   if(entrada.good()){
 
                 while (entrada.read( (char*) &linea, sizeof(linea)-1 ) ) {
-                          std::cout << linea << '\n';
+
                           if (strcmp(linea,"[  ] " ) == 0 || strcmp(linea,"[  ]\n") == 0) {
 
                              if (strcmp(linea,"[  ] " ) == 0 ){
@@ -139,9 +138,9 @@ void enumerarMapa(){
 
 
                             e1++;
-                            if (e1==4) {
+                            if (e1==5) {
                               i1++;
-                              e1 = 0;
+                              e1 = 1;
                             }
                   }
       entrada.close();
@@ -171,10 +170,9 @@ void addCord(coordenada c){
   char comb2[6] = {'[',c.x,c.y,']','\n'};
 
   if(entrada.good()){
-    std::cout << sizeof("[00] ") + sizeof('\n') << '\n';
+
                 while (entrada.read( (char*) &linea, sizeof(linea)-1 ) ) {
 
-                          std::cout << linea << '\n';
 
                           if (strcmp(linea,comb1 ) == 0 || strcmp(linea,comb2) == 0) {
 
@@ -227,6 +225,114 @@ void addCord(coordenada c){
     }
 
 }
+
+/*Eliminar Coordenada*/
+void removeCord(coordenada c){
+
+    ifstream entrada;
+    ofstream salida;
+
+    entrada.open(DIR_MAPA_1);
+    salida.open(DIR_MAPA,ios::trunc);
+
+    char linea[6];
+    int i1 = 1;
+    int e1 = 1;
+    char i2 = 1;
+    char e2 = 1;
+
+    int q2 = c.y - '1';
+
+    char k2 = (char)(q2 + 48);
+
+    char comb3[6] = {'[',c.x,k2,']',' '};
+    char comb4[6] = {'[',c.x,k2,']','\n'};
+    char comb5[6] = {"[00] "};
+    char comb6[6] = {"[00]\n"};
+    std::cout << comb3 << '\n';
+    char comb1[6];
+    char comb2[6];
+
+    bool a = false;
+    bool b = false;
+
+    if(entrada.good()){
+
+                  while (entrada.read( (char*) &linea, sizeof("[  ] ")- 1 ) ) {
+                            i2 = (char)(i1 + 48);
+                            e2 = (char)(e1 + 48);
+                            char combA[6] = {'[',i2,e2,']',' '};
+                            char combB[6] = {'[',i2,e2,']','\n'};;
+                            if (a  || b ) {
+                              std::cout << "ENTERED" << '\n';
+                              if (a) {
+                                salida << "[  ] ";
+                              }
+
+                              else {
+                                salida << "[  ]\n";
+                              }
+                            }
+
+                            else if (strcmp(linea,"[++] " ) == 0 || strcmp(linea,"[++]\n") == 0) {
+                              std::cout << "Ha entrado" << '\n';
+                              if (strcmp(linea,"[++] ") == 0) {
+                                salida << "[++] ";
+                              }
+
+                              if (strcmp(linea,"[++]\n") == 0) {
+                                salida << "[++]\n";
+                              }
+
+                            }
+
+                            else {
+                              salida << "[  ]";
+                              std::cout << "MESSAGE" << '\n';
+                              if (e1==4) {
+                                salida << endl;
+                              }
+                              else {
+                                salida << " ";
+                              }
+                            }
+
+                            if (strcmp(linea,comb3 ) == 0 || strcmp(linea, comb4 ) == 0) {
+                              std::cout << "P" << '\n';
+                              if (strcmp(linea,comb3 ) == 0) {
+                                a = true;
+                              }
+
+                              else{
+                                b = true;
+                              }
+
+                            }
+
+                            else{
+                              a = false;
+                              b = false;
+                            }
+
+                            e1++;
+                            if (e1==5) {
+                              i1++;
+                              e1 = 1;
+                            }
+
+                    }
+        entrada.close();
+        salida.close();
+      }
+
+      else {
+        std::cout << "Error 01: NO se puede leer el ARCHIVO " << DIR_MAPA << '\n';
+        entrada.close();
+        salida.close();
+      }
+
+}
+
 /*Formatear Mapa*/
 
 void formatearMapa() {
